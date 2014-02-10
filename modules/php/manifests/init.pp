9fddfd2ae84683d931714f9b3fb7c834cc8f99dd
Richard Alexander Allen <richard.allen@timeinc.com>
2014-02-10 15:57:16 -0500

    Remove peck me cache from php class.

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,11 +1,11 @@
 class php {
-	# Missing Varnish, Memcached, APC, Zend Debugger, XDebug, New Relic
+	# Missing Varnish, APC, Zend Debugger, XDebug, New Relic
 	# XDebug and Zend Debugger should be separate for ease of configuration.
 	
 	# Install PHP and all the development libraries.
 	$libraries = [
 		'php', 'php-common', 'php-devel', 'php-cli', 'php-fpm', 'php-gd', 'php-ldap',
-		'php-pear', 'php-pecl-apc', 'php-pecl-memcache',			
+		'php-pear', 'php-pecl-apc',
 		'php-mysql', 'php-soap', 'php-xml', 'php-xmlrpc', 
 		'php-pecl-apc-devel', 'php-bcmath', 'php-mcrypt',
 		'php-imap', 'php-odbc', 'php-zts'
