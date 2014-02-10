1926bd3a7f8267092ef2cf9d31b0c8e4a3d2bacc
Richard Alexander Allen <richard.alexander.allen@gmail.com>
2014-02-10 09:53:04 -0500

    Include Varnish class in main manifest, add semaphore directory.

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,7 +3,16 @@ Exec {
   path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
 }
 
+# Ensure our custom semaphore directory exists.
+# Controls which commands are run only once (on the first provision).
+file { '/tmp/vagrant-puppet-semaphore':
+	ensure => 'directory',
+	owner => 'root',
+	group => 'root'
+}		
+
 include apache
 include php
 include php::pear
 include mysql
+include varnish
