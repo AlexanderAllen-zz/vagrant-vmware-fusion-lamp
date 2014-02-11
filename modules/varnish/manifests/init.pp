b9b003fdaf3bfaddebb6c9fb95f2e667512d3452
Richard Alexander Allen <richard.allen@timeinc.com>
2014-02-10 19:25:28 -0500

    Get rid of Varnish semaphore.

--- modules/varnish/manifests/init.pp
+++ modules/varnish/manifests/init.pp
@@ -3,29 +3,29 @@ class varnish {
 	# Install the latest version of Varnish.
 	exec { 'download varnish RPM': 
 		command => 'wget http://repo.varnish-cache.org/redhat/varnish-3.0/el6/noarch/varnish-release/varnish-release-3.0-1.el6.noarch.rpm',
-		creates => '/tmp/vagrant-puppet-semaphore/varnish',
+		creates => '/usr/sbin/varnishd'
 	}
 	
 	exec { 'execute varnish RPM': 
 		command => 'rpm --nosignature -i varnish-release-3.0-1.el6.noarch.rpm', 
 		require => Exec['download varnish RPM'],
-		creates => '/tmp/vagrant-puppet-semaphore/varnish',
+		creates => '/usr/sbin/varnishd'
 	}
 	
 	package { "varnish":
-		ensure => present,
+		ensure => present
 	}
 	
 	exec { 'Set varnish chkconfig': 
 		command => 'chkconfig varnish on',
-		creates => '/tmp/vagrant-puppet-semaphore/varnish',
-		require => Package["varnish"]	
+		creates => '/usr/sbin/varnishd',
+		require => Package["varnish"]
 	}
 	
 	# Set Varnish to listen on port 8080.
 	exec { 'Update /etc/sysconfig/varnish': 
 		command => "sed -i 's/VARNISH_LISTEN_PORT=6081/VARNISH_LISTEN_PORT=80/g' /etc/sysconfig/varnish",
-		require => Package["varnish"]	
+		require => Package["varnish"]
 	}
 	
 	# sed -ir 's/port\s=\s"80"/port="8080"/g' /etc/varnish/default.vcl
@@ -34,16 +34,17 @@ class varnish {
 		require => Package["varnish"]
 	}
 	
+	/*
 	exec { 'Set varnish semaphore': 
 		command => 'touch /tmp/vagrant-puppet-semaphore/varnish', 
 		require => Package["varnish"],
-		creates => '/tmp/vagrant-puppet-semaphore/varnish',
-	}
+		creates => '/tmp/vagrant-puppet-semaphore/varnish'
+	}*/
 	
 	# Ensure Varnish is running.
 	service {
 		"varnish":
 		ensure => running,
-		require => Package["varnish"];
+		require => Package["varnish"]
 	}
 }
