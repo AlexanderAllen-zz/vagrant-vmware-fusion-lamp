e42030cf773f185edd5ce87eb2fed3a64a3e5813
Richard Alexander Allen <richard.allen@timeinc.com>
2014-02-11 17:13:05 -0500

    1.0.2 default.pp test.

--- manifests/default.pp
+++ manifests/default.pp
@@ -9,7 +9,7 @@ file { '/tmp/vagrant-puppet-semaphore':
 	ensure => 'directory',
 	owner => 'root',
 	group => 'root'
-}		
+}
 
 include apache
 include php
@@ -18,9 +18,10 @@ include php::drush
 include mysql
 include memcached
 include varnish
+include drupal
 
 # 1.0.1
-# Testing autoloading (no include)
+# Testing autoloading (no include) - WORKS
 apache::vhost {'testhost':
   port => 8080,
   docroot => '/var/www-testhost',
@@ -29,4 +30,39 @@ apache::vhost {'testhost':
 }
 
 # 1.0.2
+# Use the generic drupal:: class for variables/elements that are likely to be shared across sites.
+# That way we avoid repeating the same options many times.
+# Examples: database host, database password, server admin (overridable), global settings.php options (such as caching and proxy).
+
+
 # Create a new element for drush/drupal site that leverages apache::vhost inside.
+# Test autoload #2 - 
+drupal::site {'test.local':
+  # drush alias params, make cwd
+  #docroot => '/var/www-testhost',
+ # uri => 'default.local',
+  # make params
+  # makefile: we have a shared make file directory in ~ where we go and grab the make files from
+  # alternatively, it would be much easier if we just accepted an URL?
+  # but URLs won't work for makefiles in private repos. For that reason, for now it's easier
+  # just to create a shared dir, and let the user clone/download the make file there.
+  makefile => 'test.make',
+  #drupalroot => 'lolololo',
+}
+
+
+
+
+
+# 1.0.3
+# Have either Cocoa or Drupal manage the VHost YAML site.
+
+# VHost Hidra/YAML definition
+# apache::vhost::user_hosts
+#   -servername: "puppet"
+#     -port: 8080
+#     -docroot: "~/Sites/lsd_test/htdocs"
+#     -priority: 25
+
+# 1.0.4
+# Grab most options from drush qd and drush make from Hidra/YAML files.
